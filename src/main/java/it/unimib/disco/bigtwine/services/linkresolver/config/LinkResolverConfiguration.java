fa66e5d8f027aa627a9e548e50620c5582eb671c
Fausto <fausto91@gmail.com>
2019-07-30 15:12:38 +0200

    Configurazione kafka template

--- src/main/java/it/unimib/disco/bigtwine/services/linkresolver/config/LinkResolverConfiguration.java
+++ src/main/java/it/unimib/disco/bigtwine/services/linkresolver/config/LinkResolverConfiguration.java
@@ -1,11 +1,13 @@
 package it.unimib.disco.bigtwine.services.linkresolver.config;
 
+import it.unimib.disco.bigtwine.commons.messaging.KafkaUtils;
 import it.unimib.disco.bigtwine.services.linkresolver.executors.ExecutorFactory;
 import it.unimib.disco.bigtwine.services.linkresolver.parsers.QueryResultParserFactory;
 import it.unimib.disco.bigtwine.services.linkresolver.processors.ProcessorFactory;
 import it.unimib.disco.bigtwine.services.linkresolver.producers.QueryProducerFactory;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -19,6 +21,11 @@ import java.util.Map;
 @Configuration
 public class LinkResolverConfiguration {
 
+    @Value("${spring.cloud.stream.kafka.binder.brokers}")
+    private String kafkaBrokers;
+
+    @Value("${spring.cloud.stream.kafka.binder.defaultBrokerPort:9092}")
+    private Integer kafkaDefaultBrokerPort;
 
     @Bean
     public ExecutorFactory getExecutorFactory() {
@@ -50,8 +57,10 @@ public class LinkResolverConfiguration {
 
     @Bean
     public Map<String, Object> producerConfigs() {
+        String bootstrapServers = KafkaUtils.buildBootstrapServersConfig(this.kafkaBrokers, this.kafkaDefaultBrokerPort);
+
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         // See https://kafka.apache.org/documentation/#producerconfigs for more properties
